3de1bcb00331318b3aab57b3ea537c95ad0e98dd
1808865537@qq.com <1808865537@qq.com>
2021-01-26 23:21:55 +0800

    Initial commit

---
+++ imagerw/imager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 18088 on 2021/1/26.
+//
+
+#include "imager.h"
---
+++ imagerw/imager.h
@@ -0,0 +1,14 @@
+//
+// Created by 18088 on 2021/1/26.
+//
+
+#ifndef NORMAL_BARCODE_IMAGER_H
+#define NORMAL_BARCODE_IMAGER_H
+
+
+class imager {
+
+};
+
+
+#endif //NORMAL_BARCODE_IMAGER_H
